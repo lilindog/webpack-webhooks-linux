30193a033032e0b3cd9590b9b35492c2e9fc4e18
lilin123 <lilindog@qq.com>
2018-09-26 15:25:48 +0800

    linux上编辑json出错，提交，用windows编辑

--- webpack-webhooks-linux.js
+++ webpack-webhooks-linux.js
@@ -1,8 +1,25 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const utl = require("url");
+const url = require("url");
+const child_process = require("child_process");
 const config = JSON.parse(fs.readFileSync(path.join(__dirname, "./config.json"), "utf-8"));
+
+
+
+//执行生成sh脚本
+const pullCmd = `
+	cd ${config.sourceDir}\n 
+	git pull origin master
+`;
+const buildCmd = `
+	cd ${config.sourceDir}\n
+	webpack	
+`;
+fs.writeFileSync(path.join(__dirname, "./pull.sh", pullCmd));
+fs.eriteFileSync(path.join(__dirname, "./build.sh"), buildCmd);
+
+
 
 http.createServer((req, res)=>{
 
@@ -16,8 +33,37 @@ http.createServer((req, res)=>{
             body += chunk;
         });
         req.on("end", ()=>{
-            console.log(body);
+        	try{
+			body = JSON.parse(body);
+	        }catch(err){
+			console.log("解析请求的json数据发生错误");
+	   	}
+		
+		if(body.password === config.pass){
+			
+			//开始拉取
+			child_process.execFile(path.join(__dirname, "./pull.sh"), (err)=>{
+				if(err){
+					console.log("拉取错误");
+					console.log(err);
+				}else{
+					//执行编译
+					child_process.execFile(path.join(__dirname, "./build.sh"), (err)=>{
+						if(err){
+							console.log("weboack编译发生错误");
+							console.log(err);
+						}else{
+							console.log("编译成功");
+						}
+					});
+				}
+			});			
 
+		}else{
+			res.end("爸爸，你密码不对哦");	
+		}
+	
+	
             
         });
 
